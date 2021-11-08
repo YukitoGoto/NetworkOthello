adf8048f235f1d41f479f1b1634b3dae43e295ad
YukitoGoto <ykt3693@outlook.jp>
2021-11-09 07:55:38 +0900

    サンプルプログrム更新

--- src/ServerExample.py
+++ src/ServerExample.py
@@ -1,37 +1,41 @@
 import socket
 import pickle
+from enum import IntEnum
 
-# 横方向・縦方向のマスの数
-NUM_SQUARE = 8
-
-ip_address = '172.16.30.168'
-port = 7010
-buffer_size = 4092
-
-PLAYER1 = 1
-PLAYER2 = 2
-
-color = {
-	PLAYER1 : 'black',
-	PLAYER2 : 'white'
+# ゲーム情報
+class PlayerNum(IntEnum):
+	PLAYER1 = 1
+	PLAYER2 = 2
+PlayerColor = {
+	PlayerNum.PLAYER1 : 'black',
+	PlayerNum.PLAYER2 : 'white'
 }
+NUM_SQUARE = 8
+serverBoard = [[None] * NUM_SQUARE for i in range(NUM_SQUARE)]
 
-board = [[None] * NUM_SQUARE for i in range(NUM_SQUARE)]
+# ネットワーク情報
+PORT_NUM = 7010
+BUFFER_SIZE = 4092
 
 # Socketの作成
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-# IP Adress とPort番号をソケット割り当てる
-s.bind((socket.gethostname(), port))
+# IPアドレス・Port番号をソケット割り当てる
+s.bind((socket.gethostname(), PORT_NUM))
 # Socketの待機状態
 s.listen(5)
 
-board[4][5] = color[PLAYER1]
-
-# while Trueでクライアントからの要求を待つ
+# クライアントからの要求を待つ
 while True:
-	# 要求があれば接続の確立とソケット、アドレスを代入
+	# 要求があれば接続を確立して、ソケット・アドレスを代入
 	clientSocket, addr = s.accept()
-	msg = pickle.dumps(board)
-	# データを送信する
-	clientSocket.send(msg)
+	print(addr)
+	# クライアントからデータを受信
+	receivedBytes = clientSocket.recv(BUFFER_SIZE)
+	data = pickle.loads(receivedBytes)
+	print(data)
+	serverBoard = data
+	# クライアントにデータを送信
+	sendBytes = pickle.dumps(serverBoard)
+	clientSocket.send(sendBytes)
+	# ソケットを閉じて通信を切断
 	clientSocket.close()
